8c2e5b1c9b74ee65527cd1eff0ac5063d8d02b9e
takebayashiv-cmd <takebayashiv@gmail.com>
2026-02-11 19:55:28 -1000

    changed index.mjs to use different factory function

--- index.mjs
+++ index.mjs
@@ -13,10 +13,34 @@ function removeChildren(elem) {
   }
 }
 
-function createHandlers() {
+function createApp() {
   // put private variables here
+  const self = {
+    handleOk: handleOk,
+    handleNextPos: handleNextPos,
+    handleLocate: handleLocate,
+    handleInsert: handleInsert
+  };
 
-  // helper functions
+  // ====== API for application =======
+  function handleOk() {
+    console.log('handleOk called');
+  }
+
+  function handleNextPos() {
+
+  }
+
+  function handleLocate() {
+
+  }
+
+  async function handleInsert() {
+
+  }
+  // ======= End of API for application ===
+
+  // ======= helper functions ========
   function getNodeById(id) {
     for (let i = 0; i < node_array.length; i++) {
       const nd = node_array[i];
@@ -31,23 +55,16 @@ function createHandlers() {
       node_array[i].g.setAttribute("id", i);
     }
   }
-
-  return {
-    handleOk() {},
-
-    handleNextPos() {},
-
-    handleLocate() {},
-
-    async handleInsert() {},
-  };
+  // ======== end of helper functions =======
+  
+  return self;
 }
 
 function init() {
   console.log("init called");
-  const handlers = createHandlers();
+  const myApp = createApp();
   const ok_button = document.getElementById("ok_button");
   ok_button.addEventListener("click", () => {
-    handlers.handleOk();
+    myApp.handleOk();
   });
 }
